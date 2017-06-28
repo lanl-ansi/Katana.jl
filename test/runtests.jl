c4e160c723277990bf56d3c560b8a23c591ac9c7
Carleton <cjc@lanl.gov>
2017-06-27 22:57:09 -0600

    restoring third model in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -109,24 +109,24 @@ println("")
 ### A simple linear model with a convex objective 
 ### introduction of an auxiluary variable
 
-#m = Model(solver=katana)
+m = Model(solver=katana)
 
-#@variable(m, x)
-#@variable(m, y)
+@variable(m, x)
+@variable(m, y)
 
-#@NLobjective(m, Min, (x-1)^2 + (y-2)^2)
-#@constraint(m, x+y <= 5)
-#@constraint(m, 2*x-y <= 3)
-#@constraint(m, 3*x+9*y >= -10)
-#@constraint(m, 10*x-y >= -20)
-#@constraint(m, -x+2*y <= 8)
+@NLobjective(m, Min, (x-1)^2 + (y-2)^2)
+@constraint(m, x+y <= 5)
+@constraint(m, 2*x-y <= 3)
+@constraint(m, 3*x+9*y >= -10)
+@constraint(m, 10*x-y >= -20)
+@constraint(m, -x+2*y <= 8)
 
-#status = solve(m)
+status = solve(m)
 
-#@test status == :Optimal
-#@test isapprox(getobjectivevalue(m), 0.0, atol=opt_tol)
-#@test isapprox(getvalue(x), 1.0, atol=sol_tol)
-#@test isapprox(getvalue(y), 2.0, atol=sol_tol)
+@test status == :Optimal
+@test isapprox(getobjectivevalue(m), 0.0, atol=opt_tol)
+@test isapprox(getvalue(x), 1.0, atol=sol_tol)
+@test isapprox(getvalue(y), 2.0, atol=sol_tol)
 
 
 
