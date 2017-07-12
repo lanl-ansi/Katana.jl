0b74e6e11c8d71c689136f55ae09cbb540d9f9de
Carleton <cjc@lanl.gov>
2017-07-12 15:43:14 -0600

    adding test 501_02 for testing variants of nD shperes

--- test/misc.jl
+++ test/misc.jl
@@ -14,8 +14,35 @@
 
             #println(getobjectivevalue(m))
             #println(getvalue(vars))
-            
-            @test status == :Optimal    
+
+            @test status == :Optimal
+            @test isapprox(getobjectivevalue(m), -n/sqrt(n), atol=opt_tol)
+            for x in vars
+                @test isapprox(getvalue(x), 1/sqrt(n), atol=sol_tol)
+            end
+        end
+
+        for n in 1:20
+            nd_shpere(n)
+        end
+    end
+
+    # a.k.a. 501_02 (should converge faster than 501_01)
+    @testset "nD sphere, all variables non-zero, norm form" begin
+        function nd_shpere(n=2)
+            m = Model(solver=solver)
+
+            @variable(m, vars[1:n])
+
+            @objective(m, Min, sum(-x for x in vars))
+            @NLconstraint(m, sqrt(sum(x^2 for x in vars)) <= 1.0)
+
+            status = solve(m)
+
+            #println(getobjectivevalue(m))
+            #println(getvalue(vars))
+
+            @test status == :Optimal
             @test isapprox(getobjectivevalue(m), -n/sqrt(n), atol=opt_tol)
             for x in vars
                 @test isapprox(getvalue(x), 1/sqrt(n), atol=sol_tol)
