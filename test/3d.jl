0e88068c2bae9a4a411b1b776f4b8a570ddc30a0
Carleton <cjc@lanl.gov>
2017-08-22 14:37:55 -0600

    adding back working tests

--- test/3d.jl
+++ test/3d.jl
@@ -106,27 +106,26 @@
     end
 
     # a.k.a. 202_04
-# TODO: test fails for accuracy reasons
-#    @testset "intersection nonlinear quadratic constraints, one binding constraint, non-inflection point" begin
-#        m = Model(solver=solver)
-#
-#        @variable(m, x)
-#        @variable(m, y)
-#        @variable(m, z)
-#
-#        @objective(m, Min, x+y+2*z)
-#        @constraint(m, x^2 + y^2 <= z)
-#        @constraint(m, x^2 + y^2 <= -z+1)
-#
-#        status = solve(m)
-#
-#        @test status == :Optimal
-#        # TODO, figure out why ipopt does not ensure 1e-8 on this case
-#        @test isapprox(getobjectivevalue(m), -1/4, rtol=1e-7)
-#        @test isapprox(getvalue(x), -1/4, atol=sol_atol, rtol=sol_rtol)
-#        @test isapprox(getvalue(y), -1/4, atol=sol_atol, rtol=sol_rtol)
-#        @test isapprox(getvalue(z),  1/8, atol=sol_atol, rtol=sol_rtol)
-#    end
+    @testset "intersection nonlinear quadratic constraints, one binding constraint, non-inflection point" begin
+        m = Model(solver=solver)
+
+        @variable(m, x)
+        @variable(m, y)
+        @variable(m, z)
+
+        @objective(m, Min, x+y+2*z)
+        @constraint(m, x^2 + y^2 <= z)
+        @constraint(m, x^2 + y^2 <= -z+1)
+
+        status = solve(m)
+
+        @test status == :Optimal
+        # TODO, figure out why ipopt does not ensure 1e-8 on this case
+        @test isapprox(getobjectivevalue(m), -1/4, rtol=1e-7)
+        @test isapprox(getvalue(x), -1/4, atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(y), -1/4, atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(z),  1/8, atol=sol_atol, rtol=sol_rtol)
+    end
 
     # a.k.a. 202_05
     @testset "intersection nonlinear quadratic constraints, one binding constraint, intersection set" begin
@@ -171,48 +170,51 @@
         @test isapprox(getvalue(z), 3/4, atol=sol_atol, rtol=sol_rtol)
     end
 
-    # not supported in JuMP 0.17, try with JuMP 1.0+
-    #@testset "sqrt cone constraint (norm form), nonlinear constraint intersection" begin
-    #    m = Model(solver=solver)
+    # TODO: not supported in JuMP 0.17, try with JuMP 1.0+
+    #=
+    @testset "sqrt cone constraint (norm form), nonlinear constraint intersection" begin
+        m = Model(solver=solver)
+        @variable(m, x, start=0.1)
+        @variable(m, y, start=0.1)
+        @variable(m, z)
 
-    #    @variable(m, x, start=0.1)
-    #    @variable(m, y, start=0.1)
-    #    @variable(m, z)
+        @objective(m, Min, x+y)
+        @constraint(m, norm([x,y]) <= z-0.25)
+        @constraint(m, x^2 + y^2 <= -z+1)
 
-    #    @objective(m, Min, x+y)
-    #    @constraint(m, norm([x,y]) <= z-0.25)
-    #    @constraint(m, x^2 + y^2 <= -z+1)
+        status = solve(m)
 
-    #    status = solve(m)
-
-    #    @test status == :Optimal
-    #    @test isapprox(getobjectivevalue(m), -1/sqrt(2), atol=opt_atol, rtol=opt_rtol)
-    #    @test isapprox(getvalue(x), -sqrt(1/8), atol=sol_atol, rtol=sol_rtol)
-    #    @test isapprox(getvalue(y), -sqrt(1/8), atol=sol_atol, rtol=sol_rtol)
-    #    @test isapprox(getvalue(z), 3/4, atol=sol_atol, rtol=sol_rtol)
-    #end
+        @test status == :Optimal
+        @test isapprox(getobjectivevalue(m), -1/sqrt(2), atol=opt_atol, rtol=opt_rtol)
+        @test isapprox(getvalue(x), -sqrt(1/8), atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(y), -sqrt(1/8), atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(z), 3/4, atol=sol_atol, rtol=sol_rtol)
+    end
+    =#
 
     # a.k.a. 204_01
-# TODO: test fails with Inf on x[1]
-#    @testset "rotated second order cone, nonlinear constraint intersection" begin
-#        m = Model(solver=solver)
-#
-#        @variable(m, x)
-#        @variable(m, y)
-#        @variable(m, z >= 0, start=0.1)
-#
-#        @objective(m, Min, -y-x)
-#        @NLconstraint(m, x^2/z <= y)
-#        @constraint(m, x^2 + y^2 <= -z+1)
-#
-#        status = solve(m)
-#
-#        @test status == :Optimal
-#        @test isapprox(getobjectivevalue(m), -1.2071067837918394, atol=opt_atol, rtol=opt_rtol)
-#        @test isapprox(getvalue(x), 0.353553392657669, atol=sol_atol, rtol=sol_rtol)
-#        @test isapprox(getvalue(y), 0.8535533911341705, atol=sol_atol, rtol=sol_rtol)
-#        @test isapprox(getvalue(z), 0.14644661317207716, atol=sol_atol, rtol=sol_rtol)
-#    end
+    # TODO: test fails with Inf on x[1], issue with initial boundedness of LP
+    #=
+    @testset "rotated second order cone, nonlinear constraint intersection" begin
+        m = Model(solver=solver)
+
+        @variable(m, x)
+        @variable(m, y >= 0)
+        @variable(m, z >= 0, start=0.1)
+
+        @objective(m, Min, -y-x)
+        @NLconstraint(m, x^2/z <= y)
+        @constraint(m, x^2 + y^2 <= -z+1)
+
+        status = solve(m)
+
+        @test status == :Optimal
+        @test isapprox(getobjectivevalue(m), -1.2071067837918394, atol=opt_atol, rtol=opt_rtol)
+        @test isapprox(getvalue(x), 0.353553392657669, atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(y), 0.8535533911341705, atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(z), 0.14644661317207716, atol=sol_atol, rtol=sol_rtol)
+    end
+    =#
 
 
     # a.k.a. 205_01
@@ -239,30 +241,31 @@
 
 
     # a.k.a. 206_01
-# TODO: test fails with -Inf on x[1]
-#    @testset "power cone, nonlinear constraint intersection" begin
-#        m = Model(solver=solver)
-#
-#        # NOTE, starting any of these at 0.0 will segfault libcoinmumps
-#        @variable(m, x >= 0, start=0.1)
-#        @variable(m, y >= 0, start=0.1)
-#        @variable(m, z >= 0, start=0.1)
-#
-#        @objective(m, Max, 2*x+y+z)
-#        @NLconstraint(m, z <= x^(0.3)*y^(0.7))
-#        @NLconstraint(m, x <= z^(0.7)*y^(0.3))
-#        @constraint(m, x^2 + y^2 <= z+1)
-#
-#        status = solve(m)
-#
-#        @test status == :Optimal
-#        # TODO, figure out why ipopt does not ensure 1e-8 on this case
-#        @test isapprox(getobjectivevalue(m), 4.0, rtol=1e-7)
-#        @test isapprox(getvalue(x), 1.0, atol=sol_atol, rtol=sol_rtol)
-#        @test isapprox(getvalue(y), 1.0, rtol=1e-7)
-#        @test isapprox(getvalue(z), 1.0, rtol=1e-7)
-#    end
+    # TODO: never converges...
+    #=
+    @testset "power cone, nonlinear constraint intersection" begin
+        m = Model(solver=solver)
 
+        # NOTE, starting any of these at 0.0 will segfault libcoinmumps
+        @variable(m, x >= 0, start=0.1)
+        @variable(m, y >= 0, start=0.1)
+        @variable(m, 0 <= z <= 10, start=0.1)
+
+        @objective(m, Max, 2*x+y+z)
+        @NLconstraint(m, z <= x^(0.3)*y^(0.7))
+        @NLconstraint(m, x <= z^(0.7)*y^(0.3))
+        @constraint(m, x^2 + y^2 <= z+1)
+
+        status = solve(m)
+
+        @test status == :Optimal
+        # TODO, figure out why ipopt does not ensure 1e-8 on this case
+        @test isapprox(getobjectivevalue(m), 4.0, rtol=1e-7)
+        @test isapprox(getvalue(x), 1.0, atol=sol_atol, rtol=sol_rtol)
+        @test isapprox(getvalue(y), 1.0, rtol=1e-7)
+        @test isapprox(getvalue(z), 1.0, rtol=1e-7)
+    end
+    =#
 
     # a.k.a. 210_01
     @testset "nonlinear objective, nonlinear constraint, no binding constraints" begin
